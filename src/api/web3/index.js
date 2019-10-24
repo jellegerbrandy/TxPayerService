7e202087eb853d42358c8b3d165ec8ba01e609b1
mrrobot16 <hmgaar@gmail.com>
2019-10-24 02:31:34 -0400

    Added @truffle/hdwallet-provider module to integrate wallet mnemonic to project

--- src/api/web3/index.js
+++ src/api/web3/index.js
@@ -1,14 +1,17 @@
-import Web3 from 'web3';
+import Web3 from "web3";
+import HDWalletProvider from "@truffle/hdwallet-provider";
 import dotenv from "dotenv";
 dotenv.config();
 
 const {
   NETWORK_URL, PROVIDER_ADDRESS, PROVIDER_PRIVATE_KEY,
-  TARGET_CONTRACT_ADDRESS, TARGET_CONTRACT_METHOD
+  TARGET_CONTRACT_ADDRESS, TARGET_CONTRACT_METHOD, WALLET_MNEMONIC
 } = process.env
 
 // Instanciate Web3
-export const web3 = new Web3(new Web3.providers.HttpProvider(NETWORK_URL));
+// const provider = new Web3.providers.HttpProvider(NETWORK_URL)
+const provider = new HDWalletProvider(WALLET_MNEMONIC, NETWORK_URL);
+export const web3 = new Web3(provider);
 web3.eth.defaultAccount = PROVIDER_ADDRESS;
 
 // Web Eth
