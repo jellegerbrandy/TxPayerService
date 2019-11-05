04c471a814c16a79d01e078ec14f9342781462d6
cbrzn <cesarbrazon10@gmail.com>
2019-11-05 14:45:27 +0100

    Adding access control allow headers

--- src/app.js
+++ src/app.js
@@ -11,6 +11,11 @@ const app = express();
 
 const requestHeaders = (_, response, next) => {
   response.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  res.header("Access-Control-Allow-Credentials", true);
   next();
 };
 
